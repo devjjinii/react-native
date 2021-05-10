c06f247cd1564a1dd44db05c79e904de97ccee06
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2021-05-10 14:12:26 +0900

    section2] 완료.

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,16 +1,44 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Button } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
+ 
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+ 
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...currentGoals, {id: Math.random().toString(), value: goalTitle }]);
+    setIsAddMode(false);
+  };  // id 대신 key도 가능
+
+  const removeGoalHandler = goalId => {
+    setCourseGoals(currentGoals => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    <View style={{padding: 50}}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
-        <TextInput 
-          placeholder="Course Goal" 
-          style={{width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}} 
-          />
-        <Button title="ADD"></Button>
-      </View>
+    <View style={styles.screen}>
+      <Button title="Add New Goal" onPress={() => setIsAddMode(true)}/>
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler} />
+      <FlatList keyExtractor={(item, index) => item.id} data={courseGoals} renderItem={ itemData => (
+       <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value}/>
+      )}>
+      </FlatList>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  }
+
+});
