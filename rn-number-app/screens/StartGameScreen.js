d36168ce75687420cb55bd09ad5997f30c3d6a52
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2021-05-10 17:41:42 +0900

    feat: input값은 숫자만 허용한다. 숫자키패드 default 설정 및 style추가
    1. keyboardType="numeric"
    2. Keyboard.dismiss();

--- rn-number-app/screens/StartGameScreen.js
+++ rn-number-app/screens/StartGameScreen.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
 
 import Card from '../components/Card';
+import Color from '../constants/colors';
+import Input from '../components/Input';
 
 const StartGameScreen = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g, '')); // only 숫자만(, . 안됨)
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game~!</Text>
-            <Card style={styles.inputContainer}>
-                <View style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
-                    <TextInput />
-                    <View style={styles.buttonContainer}>
-                        <Button title="Reset" onPress={() => {}} />
-                        <Button title="Confirm" onPress={() => {}} />
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game~!</Text>
+                <Card style={styles.inputContainer}>
+                    <View style={styles.inputContainer}>
+                        <Text>Select a Number</Text>
+                        <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} 
+                            keyboardType="numeric" maxLength={2} onChangeText={numberInputHandler} value={enteredValue} />
+                        <View style={styles.buttonContainer}>
+                            <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Color.accent} /></View>
+                            <View style={styles.button}><Button title="Confirm" onPress={() => {}} color="#f7287b" /></View>
+                        </View>
                     </View>
-                </View>
-            </Card>
-        </View>
+                </Card >
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
@@ -41,6 +55,13 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'space-between',
         paddingHorizontal: 15
+    },
+    button: {
+        width: 80
+    },
+    input: {
+        width: 50,
+        textAlign: 'center'
     }
 });
 
