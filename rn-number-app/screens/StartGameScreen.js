c873af53d05b47158dc732bb60bc4879eab08c8f
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2021-05-11 13:36:41 +0900

    refactor: 입력숫자 NaN 체크 및 취소/확인 버튼 클릭시 이벤트 수정

--- rn-number-app/screens/StartGameScreen.js
+++ rn-number-app/screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
 import Card from '../components/Card';
 import Color from '../constants/colors';
@@ -8,10 +8,34 @@ import Input from '../components/Input';
 const StartGameScreen = props => {
 
     const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
 
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, '')); // only 숫자만(, . 안됨)
     };
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    };
+    
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if(isNaN(chosenNumber) || chosenNumber <=0 || chosenNumber > 99) {
+            Alert.alert('Invalid number!','Number has to be a number between 1 and 99', [{text: 'Okay', style: 'destructive', onPress: resetInputHandler}]);
+            return;
+        }
+        setConfirmed(true);
+        setSelectedNumber(parseInt(enteredValue));
+        setEnteredValue('');
+    };
+
+    let confirmedOutput;
+
+    if(confirmed) {
+        confirmedOutput = <Text>Chosen Number : {selectedNumber} </Text>
+    }
 
     return (
         <TouchableWithoutFeedback onPress={() => {
@@ -25,11 +49,12 @@ const StartGameScreen = props => {
                         <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} 
                             keyboardType="numeric" maxLength={2} onChangeText={numberInputHandler} value={enteredValue} />
                         <View style={styles.buttonContainer}>
-                            <View style={styles.button}><Button title="Reset" onPress={() => {}} color={Color.accent} /></View>
-                            <View style={styles.button}><Button title="Confirm" onPress={() => {}} color="#f7287b" /></View>
+                            <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Color.accent} /></View>
+                            <View style={styles.button}><Button title="Confirm" onPress={confirmInputHandler} color="#f7287b" /></View>
                         </View>
                     </View>
                 </Card >
+                {confirmedOutput}
             </View>
         </TouchableWithoutFeedback>
     );
