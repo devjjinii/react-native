c8440cffcf9877f285224b54a96f18eae2abd5a1
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2021-05-25 17:10:43 +0900

    feat: 좋아요 기능 추가 중

--- rn-meals-app/components/CategoryGridTile.js
+++ rn-meals-app/components/CategoryGridTile.js
@@ -13,7 +13,7 @@ const CategoryGridTile = props => {
         <View style={styles.gridItem} >
             <TouchableCmp style={{flex: 1}} onPress={props.onSelect}>
                 <View style={{...styles.container, ...{backgroundColor: props.color} }}>
-                    <Text stlye={styles.title} numberOfLines={2}>{props.title}</Text>
+                    <Text style={styles.title} numberOfLines={2}>{props.title}</Text>
                 </View>
             </TouchableCmp>
         </View>
@@ -27,7 +27,9 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
+        overflow: Platform.OS === 'android' && Platform.Version == 21 ? 'hidden' : 'visible',
+        elevation: 5,
+        
     },
     container: {
         flex: 1,
@@ -36,7 +38,6 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.26,
         shadowOffset: { width:0, height:2 },
         shadowRadius: 10,
-        elevation: 3,
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
