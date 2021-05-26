101ea922ee90c009cdf1bc5ce8372c5504abda02
YuJin Choi <53853730+devjjinii@users.noreply.github.com>
2021-05-26 17:30:23 +0900

    feat: top, bottom navigation 구현 중

--- rn-meals-app/navigation/MealsNavigation.js
+++ rn-meals-app/navigation/MealsNavigation.js
@@ -1,31 +1,84 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createStackNavigator} from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
-const MealsNavigator = createStackNavigator({
-    Categories: {
-        screen: CategoriesScreen, 
-        navigationOptions: {
-            headerTitle: 'Meal Categories', 
-        }
+const defaultStackNaviOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
     },
-    CategoryMeals: {
-        screen: CategoryMealsScreen,
-    },
-    MealDetail: MealDetailScreen
-}, {
-    // initialRouteName: 'MealDetail',
-    defaultNavigationOptions : {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+};
+
+const MealsNavigator = createStackNavigator(
+    {
+        Categories: {
+            screen: CategoriesScreen
         },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+        CategoryMeals: {
+            screen: CategoryMealsScreen
+        },
+        MealDetail: MealDetailScreen
+    }, {
+         // initialRouteName: 'MealDetail',
+        defaultNavigationOptions : defaultStackNaviOptions
+    });
+
+const FavNavigator = createStackNavigator(
+    {
+       Favorites: FavoritesScreen,
+       MealDetail: MealDetailScreen
+    }, {
+         // initialRouteName: 'MealDetail',
+        defaultNavigationOptions : defaultStackNaviOptions
+    }
+);
+
+const tabScreenConfig = {
+    Meals: {
+        screen: MealsNavigator, 
+        navigationOptions : {
+            tabBarIcon: tabInfo => {
+                return (
+                    <Icon name='ios-restaurant' size={25} color={tabInfo.tintColor}/>
+                );
+            },
+            tabBarColor: Colors.primaryColor
+        }},
+    Favorites: {
+        screen: FavNavigator,
+        navigationOptions: {
+            tabBarIcon: tabInfo => {
+                return (
+                    <Icon name='ios-star' size={25} color={tabInfo.tintColor}/>
+                );
+            },
+            tabBarColor: Colors.accentColor
+        }
+    }
+};
+
+const MealsFavTabNavigator = Platform.OS === 'android' ? 
+createMaterialBottomTabNavigator(tabScreenConfig, {
+    activeTintColor: 'white',
+    shifting: true,
+    barStyle: {
+        backgroundColor: Colors.accentColor
+    }
+}) 
+: createBottomTabNavigator(tabScreenConfig, {
+    tabBarOptions: {
+        activeTintColor: Colors.accentColor
     }
 });
 
-export default createAppContainer(MealsNavigator);
+export default createAppContainer(MealsFavTabNavigator);
--- rn-meals-app/screens/CategoryMealsScreen.js
+++ rn-meals-app/screens/CategoryMealsScreen.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import { View, FlatList, StyleSheet} from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
-import MealItem from '../components/MealItem';
+import MealList from '../components/MealList';
 
 const CategoryMealsScreen = props => {
-
-    const renderMealItem = itemData => {
-        return <MealItem title={itemData.item.title} 
-        image={itemData.item.imageUrl}
-        duration={itemData.item.duration} 
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-        onSelectMeal={() => {
-            props.navigation.navigate({routeName: 'MealDetail', params: {
-                mealId: itemData.item.id
-
-            }})
-        }}/>;
-    };
-
     const catId = props.navigation.getParam('categoryId');
 
     // const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
@@ -28,14 +12,7 @@ const CategoryMealsScreen = props => {
         meal => meal.categoryIds.indexOf(catId) >= 0
     );
 
-    return (
-        <View style={styles.screen}>
-           <FlatList data={displayedMeals} 
-            keyExtractor={(item, index) => item.id} 
-            renderItem={renderMealItem}
-            style={{width: '100%'}}/>
-        </View>
-    );
+    return <MealList listData={displayedMeals} navigation={props.navigation}/>;
 };
 
 CategoryMealsScreen.navigationOptions = navigationData => {
@@ -47,13 +24,5 @@ CategoryMealsScreen.navigationOptions = navigationData => {
         headerTitle: selectedCategory.title
     };
 };
-
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-});
 
 export default CategoryMealsScreen;
